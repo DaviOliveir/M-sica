47626357d2284e64b884c1e1b123a8a0c33077dc
davi jose batista lima de oliveira <davibatistalimadeoliveira@gmail.com>
2022-09-02 22:22:51 -0300

    musica finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Palavras não bastam, não dá pra entender");
+Console.WriteLine("E esse medo que cresce não para");
+Console.WriteLine("É uma história que se complicou");
+Console.WriteLine("Eu sei bem o porquê");
+
+Console.WriteLine("Qual é o peso da culpa que eu carrego nos braços");
+Console.WriteLine("Me entorta as costas e dá um cansaço");
+Console.WriteLine("A maldade do tempo fez eu me afastar de você");
+
+Console.WriteLine("E quando chega a noite e eu não consigo dormi");
+Console.WriteLine("Meu coração acelera e eu sozinha aqui");
+Console.WriteLine("Eu mudo o lado da cama, eu ligo a televisão");
+console.writeline("Olhos nos olhos no espelho e o telefone na mão");
